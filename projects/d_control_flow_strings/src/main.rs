5242b3bb57c70bd0c4e3caff85977580acf345cd
biohackerellie <epkerns@gmail.com>
2024-08-11 10:45:07 -0600

    lesson d

--- projects/d_control_flow_strings/src/main.rs
+++ projects/d_control_flow_strings/src/main.rs
@@ -15,11 +15,14 @@ fn main() {
     // This consumes the `args` vector to iterate through each String
     for arg in args {
         // 1a. Your task: handle the command-line arguments!
-        //
-        // - If arg is "sum", then call the sum() function
-        // - If arg is "double", then call the double() function
-        // - If arg is anything else, then call the count() function, passing "arg" to it.
 
+        if arg == "sum" {
+            sum();
+        } else if arg == "double" {
+            double();
+        } else {
+            count(arg)
+        }
 
         // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
         // after "cargo run".  For example "cargo run sum"
@@ -32,6 +35,9 @@ fn sum() {
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
 
+    for i in 7..=23 {
+        sum += i;
+    }
 
     println!("The sum is {}", sum);
 }
@@ -42,9 +48,14 @@ fn double() {
     // 3. Use a "while loop" to count how many times you can double the value of `x` (multiply `x`
     // by 2) until `x` is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
-
-
-    println!("You can double x {} times until x is larger than 500", count);
+    while x <= 500 {
+        x *= 2;
+        count += 1;
+    }
+    println!(
+        "You can double x {} times until x is larger than 500",
+        count
+    );
 }
 
 fn count(arg: String) {
@@ -52,7 +63,15 @@ fn count(arg: String) {
     // You will need to count your loops, somehow.  Run it with `cargo run bananas`
     //
     // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
-
+    let mut count = 0;
+    loop {
+        if count < 8 {
+            count += 1;
+        } else {
+            break;
+        }
+        print!("{} ", arg);
+    }
 
     println!(); // This will output just a newline at the end for cleanliness.
 }
