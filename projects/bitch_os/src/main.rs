305c158dd36cb1b72676e95275542dcb5f593db4
biohackerellie <epkerns@gmail.com>
2024-08-12 09:39:46 -0600

    os

---
+++ projects/bitch_os/src/main.rs
@@ -0,0 +1,16 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+// called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle] // don't mangle func name
+pub extern "C" fn _start() -> ! {
+    // entry point for kernel
+    loop {}
+}
